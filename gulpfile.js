bf9928cbce0273bbf5dd1aa919bc4eec2d8be43f
Randall Sexton <jrandallsexton@gmail.com>
2015-08-04 08:10:04 -0500

    update gulpfile; jquery local

--- gulpfile.js
+++ gulpfile.js
@@ -24,5 +24,23 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('dist'));
 });
 
+gulp.task('deploy', function() {
+	gulp.src('css/*.css').pipe(gulp.dest('release/css'));
+	gulp.src('css/colors/*.css').pipe(gulp.dest('release/css/colors'));
+	gulp.src('fonts/*.*').pipe(gulp.dest('/fonts'));
+	gulp.src('img/*.*').pipe(gulp.dest('release/img'));
+	gulp.src('img/logos/*.*').pipe(gulp.dest('release/img/logos'));
+	gulp.src('img/overlays/*.*').pipe(gulp.dest('release/img/overlays'));
+	gulp.src('img/portfolio/large/*.*').pipe(gulp.dest('release/img/portfolio/large'));
+	gulp.src('img/portfolio/small/*.*').pipe(gulp.dest('release/img/portfolio/small'));
+	gulp.src('img/skills/*.*').pipe(gulp.dest('release/img/skills'));
+	gulp.src('img/slider/*.*').pipe(gulp.dest('release/img/slider'));
+	gulp.src('img/slider/cha/*.*').pipe(gulp.dest('release/img/slider/cha'));
+	gulp.src('js/*.js').pipe(gulp.dest('release/js'));
+	gulp.src('*.ico').pipe(gulp.dest('release'));
+	gulp.src('*.html').pipe(gulp.dest('release'));
+	return;
+});
+
 // Default Task
-gulp.task('default', ['lint', 'scripts']);
+gulp.task('default', ['lint', 'scripts', 'deploy']);
